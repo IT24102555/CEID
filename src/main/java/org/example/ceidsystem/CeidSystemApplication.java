1c0be777277d60c050f99bd085f05f70d5fe8c1a
Bisheka <weerathungabisheka@gmail.com>
2025-10-19 19:45:31 +0530

    initial commit

---
+++ src/main/java/org/example/ceidsystem/CeidSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.ceidsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeidSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeidSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ceidsystem/CeidSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ceidsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeidSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
